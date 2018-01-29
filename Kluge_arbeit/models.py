9c80c3d258c1efe3d1497557d44c61ee1a2cb4c1
amokr <amokrane.zatout@parisnanterre.fr>
2018-01-29 22:34:22 +0100

    model

--- Kluge_arbeit/models.py
+++ Kluge_arbeit/models.py
@@ -3,3 +3,8 @@
 
 from django.db import models
 
+class Utilisateur(models.Model):
+    id_utilisateur = models.CharField(max_length=50,null=True, blank=True)
+    name=models.CharField(max_length=100000,null=True, blank=True)
+
+
