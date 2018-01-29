44baf968dad08bb3f2ffd1a4a19d7f6d439de5c5
amokrane-zatout <amokrane.zatout@parisnanterre.fr>
2018-01-29 11:42:23 +0100

    Update settings.py

--- klugeArbeit/settings.py
+++ klugeArbeit/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    #'django.contrib.admin',
+    'Kluge_arbeit.apps.KlugeArbeitConfigConfig',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
