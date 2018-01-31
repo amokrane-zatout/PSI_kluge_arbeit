9caffb5c13df91e4c6dc4726b25780727dcd0201
nadiamas <nadia.maslouhi@gmail.com>
2018-01-31 14:54:39 +0100

    add script insertion data Base

---
+++ scripts/data.sql
@@ -0,0 +1,5 @@
+insert into Kluge_arbeit_utilisateur VALUES(1,'admin','toto','toto','toto@gmail.com','informatique',35,1,1,0,0,5,0);
+insert into Kluge_arbeit_utilisateur VALUES(2,'nadia','nadia','maslouhi','nadia.maslouhi@gmail.com','informatique',35,1,1,0,0,5,0);
+insert into Kluge_arbeit_matiere VALUES (1,'GDA',1,0,12);
+insert into Kluge_arbeit_chapitre VALUES (1,'HBASE',0,1,1);
+insert into Kluge_arbeit_chapitre VALUES (2,'CouchDB',1,2,1);
